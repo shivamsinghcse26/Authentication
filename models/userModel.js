1b3b9caab5a665992504f0b6a6caac3ce9c621f8
shivam <000shiv2304@gmail.com>
2026-04-17 22:22:25 +0530

    "initial commit"

---
+++ models/userModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const  userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"UserName is required"],
+        
+    },
+    email:{
+        type:String,
+        required:[true,"Email is required"],
+        unique:[true,"Email must be unique"]
+
+    },
+    password:{
+        type:String,
+        required:[true,"Password is required"],
+       
+    }
+})
+const User = mongoose.model("User",userSchema);
+
+export default User;
